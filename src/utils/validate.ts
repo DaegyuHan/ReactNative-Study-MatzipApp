b7251b3cfe25044e52012e21923014e06f78c9c0
Daegyu Han <bigstar77han@gmail.com>
2025-04-01 02:03:53 +0900

    [FEAT] useFrom Customizing 을 활용한 Signup validate 기능 구현

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -3,20 +3,42 @@ type UserInformation = {
     password: string;
 }
 
-function validateLogin(values: UserInformation) {
+function validateUser(values: UserInformation) {
     const errors = {
         email: '',
         password: '',
     };
 
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+    // 이메일 검증
+    if (!values.email) {
+        errors.email = '이메일을 입력해주세요';
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
         errors.email = '올바른 이메일 형식이 아닙니다.';
     }
-    if (!(values.password.length >= 8 && values.password.length < 20)) {
+
+    // 비밀번호 검증
+    if (!values.password) {
+        errors.password = '비밀번호를 입력해주세요.';
+    } else if (!(values.password.length >= 8 && values.password.length < 20)) {
         errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
     }
 
     return errors;
 }
 
-export {validateLogin}
+function validateLogin(values: UserInformation) {
+    return validateUser(values);
+}
+
+function validateSignup(values: UserInformation & {passwordConfirm: string}) {
+    const errors = validateUser(values);
+    const signupErrors = {...errors, passwordConfirm: ''};
+
+    if (values.password !== values.passwordConfirm) {
+        signupErrors.passwordConfirm = '비밀번호가 일치하지 않습니다.';
+    }
+
+    return signupErrors;
+}
+
+export {validateLogin, validateSignup};
