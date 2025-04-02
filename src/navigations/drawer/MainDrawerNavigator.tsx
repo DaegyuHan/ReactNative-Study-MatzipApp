aece953da6ab4cdbb187abf2caf4df57002070fd
Daegyu Han <bigstar77han@gmail.com>
2025-04-02 15:53:05 +0900

    [FEAT] Google Map API 연동 및 MapHomeScreen 출력

--- src/navigations/drawer/MainDrawerNavigator.tsx
+++ src/navigations/drawer/MainDrawerNavigator.tsx
@@ -1,4 +1,4 @@
-import {createDrawerNavigator} from "@react-navigation/drawer";
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import MapHomeScreen from '@/screens/map/MapHomeScreen.tsx';
 import CalenderHomeScreen from '@/screens/calender/CalenderHomeScreen.tsx';
 import FeedHomeScreen from '@/screens/feed/FeedHomeScreen.tsx';
@@ -6,13 +6,13 @@ import FeedHomeScreen from '@/screens/feed/FeedHomeScreen.tsx';
 const Drawer = createDrawerNavigator();
 
 function MainDrawerNavigator() {
-    return (
-        <Drawer.Navigator>
-            <Drawer.Screen name="MapHome" component={MapHomeScreen} />
-            <Drawer.Screen name="FeedHome" component={FeedHomeScreen} />
-            <Drawer.Screen name="CalenderHome" component={CalenderHomeScreen} />
-        </Drawer.Navigator>
-    );
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="MapHome" component={MapHomeScreen} />
+      <Drawer.Screen name="FeedHome" component={FeedHomeScreen} />
+      <Drawer.Screen name="CalenderHome" component={CalenderHomeScreen} />
+    </Drawer.Navigator>
+  );
 }
 
 export default MainDrawerNavigator;
